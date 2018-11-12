d584058f895fb524585a9fdfec6169b0056a0b55
Ian Kelly <ian.aloysious.kelly@gmail.com>
2018-11-12 12:24:43 -0600

    Update FooterPane.vue

--- src/components/FooterPane.vue
+++ src/components/FooterPane.vue
@@ -21,7 +21,7 @@
     </div>
     <div class="px-4">
       <div class="container mx-auto pt-4 pb-6 items-center flex">
-        <img src="../assets/codepilot-logo-dark.png" alt="CodePilot logo" class="h-10">
+        <img src="../assets/codepilot-logo-dark.png" alt="datahook.io logo" class="h-10">
         <span class="uppercase text-grey-dark pt-2 pl-4">– All rights reserved</span>
       </div>
     </div>
